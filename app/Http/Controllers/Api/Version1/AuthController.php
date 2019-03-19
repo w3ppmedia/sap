26358112e1955ff44a90d5073cc1b129914f365c
Cyril Nxumalo <ict.cyril@gmail.com>
2019-03-19 13:13:11 +0200

    Getting session from client

--- app/Http/Controllers/Api/Version1/AuthController.php
+++ app/Http/Controllers/Api/Version1/AuthController.php
@@ -27,9 +27,9 @@ class AuthController extends Controller
      */
     public function login(AuthLoginRequest $authRequest) {
         $authRequest = $authRequest->all();
-        $sessionId = $this->connector->createClientSession($authRequest);
+        $this->connector->createClientSession($authRequest);
 
-        return response()->json(['SessionId' => $sessionId]);
+        return response()->json(['SessionId' => $this->connector->getClientSession()]);
     }
 
     /**
